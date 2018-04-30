0f48a55b0248f20dc18e155023764bcfec9a431a
Colin <ymcacolin@gmail.com>
2018-04-30 08:53:37 -0700

    Player slide everywhere anymore

--- Stay On The Screen/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Stay On The Screen/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -14,8 +14,12 @@ namespace UnityStandardAssets._2D
         private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
         [SerializeField]
         private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
+        [SerializeField]
+        private PhysicsMaterial2D Slippery;
 
         private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
+        private Transform RightCheck;
+        private Transform LeftCheck;
         private bool m_Grounded;            // Whether or not the player is grounded.
         private Rigidbody2D m_Rigidbody2D;
         private bool m_FacingRight = true;  // For determining which way the player is currently facing.
@@ -25,14 +29,15 @@ namespace UnityStandardAssets._2D
             // Setting up references.
             m_GroundCheck = transform.Find("GroundCheck");
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
-
+            RightCheck = transform.Find("RightCheck");
+            LeftCheck = transform.Find("LeftCheck");
         }
 
 
         private void FixedUpdate()
         {
             m_Grounded = false;
-            RaycastHit2D hit = Physics2D.BoxCast(m_GroundCheck.position, new Vector2(0.99f, 0.1f), this.transform.eulerAngles.z, new Vector2(0, -1), 0, 1 << LayerMask.NameToLayer("Platform"), -Mathf.Infinity, Mathf.Infinity);
+            RaycastHit2D hit = Physics2D.BoxCast(m_GroundCheck.position, new Vector2(0.99f, 0.1f), 0, new Vector2(0, -1), 0, 1 << LayerMask.NameToLayer("Platform"), -Mathf.Infinity, Mathf.Infinity);
             if (hit.collider != null)
             {
                 m_Grounded = true;
@@ -49,21 +54,17 @@ namespace UnityStandardAssets._2D
             //only control the player if grounded or airControl is turned on
             if (m_Grounded || m_AirControl)
             {
+                RaycastHit2D hitRight = Physics2D.BoxCast(RightCheck.position, new Vector2(0.1f, 0.99f), 0, new Vector2(1, 0), 0, 1 << LayerMask.NameToLayer("Platform"), -Mathf.Infinity, Mathf.Infinity);
+                RaycastHit2D hitLeft = Physics2D.BoxCast(LeftCheck.position, new Vector2(0.1f, 0.99f), 0, new Vector2(-1, 0), 0, 1 << LayerMask.NameToLayer("Platform"), -Mathf.Infinity, Mathf.Infinity);
+
+                if(hitRight.collider != null || hitLeft.collider != null){
+                    m_Rigidbody2D.sharedMaterial = Slippery;
+                } else{ 
+                    m_Rigidbody2D.sharedMaterial = null;
+                }
                 // Move the character
                 m_Rigidbody2D.velocity = new Vector2(move * m_MaxSpeed, m_Rigidbody2D.velocity.y);
 
-                // If the input is moving the player right and the player is facing left...
-                if (move > 0 && !m_FacingRight)
-                {
-                    // ... flip the player.
-                    Flip();
-                }
-                // Otherwise if the input is moving the player left and the player is facing right...
-                else if (move < 0 && m_FacingRight)
-                {
-                    // ... flip the player.
-                    Flip();
-                }
             }
             // If the player should jump...
             if (m_Grounded && jump)
@@ -75,15 +76,6 @@ namespace UnityStandardAssets._2D
         }
 
 
-        private void Flip()
-        {
-            // Switch the way the player is labelled as facing.
-            m_FacingRight = !m_FacingRight;
-
-            // Multiply the player's x local scale by -1.
-            Vector3 theScale = transform.localScale;
-            theScale.x *= -1;
-            transform.localScale = theScale;
-        }
+ 
     }
 }
