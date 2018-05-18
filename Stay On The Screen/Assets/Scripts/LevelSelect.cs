6883335f65942598894afc92bddb4ce8c422d6d8
Colin <ymcacolin@gmail.com>
2018-05-18 08:54:17 -0700

    Added level select

---
+++ Stay On The Screen/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class LevelSelect : MonoBehaviour
+{
+    public Button[] buttonsarr;
+    public void Start()
+    {
+        for (int i = 0; i < buttonsarr.Length; i++)
+        {
+            int x = i;
+            buttonsarr[i].onClick.AddListener(delegate { SelectLevel(x + 1); });
+        }
+    }
+    public void SelectLevel(int i)
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + i;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
+
